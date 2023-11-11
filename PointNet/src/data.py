d6a8e5f46990f6d0698a82a05d68068f4e74f306
Brian <b.lee@utexas.edu>
2023-11-11 19:34:03 +0900

    added data sampling module at the end

--- PointNet/src/data.py
+++ PointNet/src/data.py
@@ -3,3 +3,24 @@
 from torch_geometric.datasets import ShapeNet
 
 dataset = ShapeNet(root='./ShapeNet', categories=['Table', 'Lamp', 'Guitar', 'Motorbike']).shuffle()[:5000]
+
+# %%
+if __name__=='__main__':
+    from torch_geometric.data import DataLoader
+    from torch_geometric.datasets import ShapeNet
+    from paths import DATA
+    import numpy as np
+
+    categories = ['Table', 'Lamp', 'Guitar', 'Motorbike']
+    n_dataset = 100
+
+    # Load ShapeNet Dataset
+    dataset = ShapeNet(root=DATA, categories=categories).shuffle()[:n_dataset]
+    # train_dataloader = DataLoader(dataset        = dataset,
+    #                               batch_size     = 32,
+    #                               shuffle        = True,
+    #                               num_workers    = 12,
+    #                               pin_memory     = True) # pin_memory=True to keep the data in GPU
+
+    random_index = np.random.randint(0, len(dataset))
+    print(dataset[random_index])

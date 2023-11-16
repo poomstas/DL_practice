68f7c90405f94a6e5c8946c9558b3ad6ddf777d4
Brian <b.lee@utexas.edu>
2023-11-17 07:42:25 +0900

    started writing test code. need to write accuracy calculation

---
+++ PointNet2/test.py
@@ -0,0 +1,50 @@
+# %%
+import os
+import torch
+import random
+
+from paths import DATA
+from train import TrainPointNet2
+
+import torch_geometric.transforms as T
+from torch_geometric.loader import DataLoader
+from torch_geometric.datasets import ModelNet
+from torch_geometric.data import Data, Batch
+
+from util import plot_3d_shape
+
+# %%
+CKPT_PATH = '/home/brian/github/DL_practice/PointNet2/model_checkpoint/aorus_20231116_114443/'
+CKPT_FILENAME = 'epoch=14-loss=0.03583.ckpt' 
+
+trainer = TrainPointNet2.load_from_checkpoint(os.path.join(CKPT_PATH, CKPT_FILENAME), map_location=torch.device('cpu'))
+
+dataset_val   = ModelNet(root             = DATA,
+                         train            = False,
+                         name             = '10',
+                         pre_transform    = T.NormalizeScale(),
+                         transform        = T.SamplePoints(1024))
+
+
+val_dataloader = DataLoader(dataset        = dataset_val,
+                            batch_size     = 16,
+                            shuffle        = True,
+                            num_workers    = 8,
+                            pin_memory     = False)
+
+
+# %% Plot 3D point cloud and print pred & actual
+random_index = random.choice(range(len(dataset_val)))
+print('='*90)
+plot_3d_shape(dataset_val[random_index])
+
+single_batch = Batch.from_data_list([dataset_val[random_index]])
+out = trainer(single_batch)
+pred = torch.argmax(out, dim=1)
+actual = single_batch.y
+
+print('Predicted:\t', pred)
+print('Actual:\t\t', actual)
+
+
+# %% Calculate accuracy

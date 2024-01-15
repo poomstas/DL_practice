b4a9e96a0d000fc6d456e9e70aeae1bfdaedce13
brian <b.lee@utexas.edu>
2024-01-15 11:26:59 +0900

    added COCO download script

---
+++ datasets/COCO/download_COCO_2017.sh
@@ -0,0 +1,15 @@
+(computer_vision) brian@brian:~/Downloads/COCO$ wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+--2024-01-15 11:00:18--  http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+# images
+wget -c http://images.cocodataset.org/zips/train2017.zip   # train dataset
+wget -c http://images.cocodataset.org/zips/val2017.zip     # validation dataset
+wget -c http://images.cocodataset.org/zips/test2017.zip    # test dataset
+wget -c http://images.cocodataset.org/zips/unlabeled2017.zip
+
+
+# annotations
+wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget -c http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
+wget -c http://images.cocodataset.org/annotations/image_info_test2017.zip
+wget -c http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
+
